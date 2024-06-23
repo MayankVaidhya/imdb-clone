d8e31d9746dc827ed53a62fc613aa60e50d2fa97
MayankVaidhya <vaidhyamayank490@gmail.com>
2024-06-23 20:20:22 +0530

    making some changes

--- components/Card.jsx
+++ components/Card.jsx
@@ -4,7 +4,7 @@ import { FiThumbsUp } from "react-icons/fi";
 
 export default function Card({ result }) {
   return (
-    <div className="group cursor-pointer sm:hover:shadow-slate-400 sm:shadow-md rounded-lg sm:border sm:border-slate-400 sm:m-2 transition-shadow duration-200">
+    <div className="group cursor-pointer shadow-slate-400 sm:hover:shadow-slate-400 shadow-md sm:shadow-md rounded-lg border sm:border border-slate-400 sm:border-slate-400 m-2 sm:m-2 transition-shadow duration-200">
       <Link href={`/movie/${result.id}`}>
         <Image
           src={`https://image.tmdb.org/t/p/original/${
@@ -15,7 +15,7 @@ export default function Card({ result }) {
           height={300}
           className="sm:rounded-t-lg group-hover:opacity-75 transition-opacity duration-300"
         ></Image>
-        <div className="">
+        <div className="p-2">
           <p className="line-clamp-2 text-md">{result.overview}</p>
           <h2 className="text-lg font-bold truncate">
             {result.title || result.name}
