52427367a8eaca2e1174fa2ec77ff1192e134ccf
MayankVaidhya <vaidhyamayank490@gmail.com>
2024-06-23 12:35:18 +0530

    designing contact us page

---
+++ app/contact/page.jsx
@@ -0,0 +1,20 @@
+export default function Contact() {
+  return (
+    <div className="max-w-6xl mx-auto p-3 space-y-4">
+      <h1 className="text-2xl font-medium text-amber-600">Contact Page</h1>
+      <p>
+        We value your feedback and inquiries. For any questions, suggestions, or
+        support, please do not hesitate to reach out to us. Our team is
+        dedicated to providing prompt assistance and ensuring your experience
+        with us is positive. You can contact us via email at:
+      </p>
+      <p>
+        Email: <span className="text-amber-500">imdb@imdb.com</span>
+      </p>
+      <p>
+        We strive to respond to all emails within 24-48 hours. Thank you for
+        connecting with us!
+      </p>
+    </div>
+  );
+}
